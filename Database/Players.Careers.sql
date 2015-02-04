f51a033ec5fdd61ab36c2084a9c487e178530268
jchansp <jciansp@hotmail.com>
2015-02-04 16:54:14 +0100

    Players and Teams tables created

---
+++ Database/Players.Careers.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [Players].[Careers]
+(
+	[PlayerId] UNIQUEIDENTIFIER NOT NULL REFERENCES [dbo].[Players]([Id]), 
+    [TeamId] UNIQUEIDENTIFIER NOT NULL REFERENCES [dbo].[Teams]([Id]), 
+    [Date] DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+    PRIMARY KEY ([Date], [PlayerId], [TeamId])
+)
